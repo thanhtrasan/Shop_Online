e9d4f34d03139786487fa26881dabe448a0d9a86
Trần Trung Trạng <33047158+trantrungtrang@users.noreply.github.com>
2018-05-15 16:19:22 +0700

    tích hợp template vào express

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/home.html', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/menu.html', function(req, res, next) {
+  res.render('menu', { title: 'Menu' });
+});
+router.get('/pizzas.html', function(req, res, next) {
+  res.render('pizzas', { title: 'Pizzas' });
+});
+router.get('/about.html', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+router.get('/blog.html', function(req, res, next) {
+  res.render('blog', { title: 'Blog' });
+});
+router.get('/contact.html', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
