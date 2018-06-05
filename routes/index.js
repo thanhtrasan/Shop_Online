f4978c1fc1c8fa94040a412a76cd1f52ffd6938b
Trần Trung Trạng <33047158+trantrungtrang@users.noreply.github.com>
2018-06-05 19:23:03 +0700

    chỉnh sửa file index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+router.get('/', function (req, res) {
+  res.render('index');
 });
+
+router.get('/shop', function (req, res) {
+  res.render('shop');
+});
+
+router.get('/about', function (req, res) {
+  res.render('about');
+});
+
+router.get('/add-to-wishlist', function (req, res) {
+  res.render('add-to-wishlist');
+});
+
+router.get('/blog', function (req, res) {
+  res.render('blog');
+});
+
+router.get('/contact', function (req, res) {
+  res.render('contact');
+});
+
+router.get('/cart', function (req, res) {
+  res.render('cart');
+});
+
+router.get('/checkout', function (req, res) {
+  res.render('checkout');
+});
+
+router.get('/order-complete', function (req, res) {
+  res.render('order-complete');
+});
+
+router.get('/product-detail', function (req, res) {
+  res.render('product-detail');
+});
+
 
 module.exports = router;
