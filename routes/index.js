daa5508432f5c85d708bdc54153953165e343ba7
DESKTOP-VTCA649\duoipham <trangpn9a4@gmail.com>
2018-06-10 00:13:37 +0700

    Đưa dữ liệu vào áo jacket

--- routes/index.js
+++ routes/index.js
@@ -44,6 +44,19 @@ app.get('/shop/hoodie', function (req, res) {
 });
 
 
+app.get('/shop/jacket', function (req, res) {
+  
+  var query="select * from quanao where maquanao like '%JK%'";
+  db.load(query).then(
+  rows=>{
+      var vm={
+          ttsp:rows
+      };
+        res.render('shop',vm);
+  })
+});
+
+
 app.get('/about', function (req, res) {
   res.render('about');
 });
