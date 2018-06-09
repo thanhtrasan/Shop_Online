1c19be63c97fba138848ad27636e157e81fb101b
DESKTOP-VTCA649\duoipham <trangpn9a4@gmail.com>
2018-06-09 14:43:24 +0700

    Liên kết cơ sở dữ liệu trong mysql

---
+++ fn/db.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+var Promise = require('promise');
+
+
+exports.load = sql => {
+    return new Promise((resolve, reject) => {
+        var cn = mysql.createConnection({
+            host: 'db4free.net',
+            port: 3306,
+            user: 'qlquanao',
+            password: 'qlquanao',
+            database: 'qlquanao'
+        });
+
+        cn.connect();
+
+        cn.query(sql, function(error, rows, fields) {
+            if (error) {
+            	reject(error);
+            } else {
+            	resolve(rows);
+            }
+
+            cn.end();
+        });
+    });
+}
