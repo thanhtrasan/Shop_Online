10a67aea2dbf98ede7fd823cdb6d44aa34a521b7
Tra San <thanhtrasan@gmail.com>
2018-06-28 18:55:06 +0700

    thêm chức năng và chỉnh sửa giao diện admin
    sửa giao diện admin
    hoàn thành chức năng thêm xóa sửa sản phẩm

--- models/admin/quanao_model.js
+++ models/admin/quanao_model.js
@@ -2,7 +2,7 @@ var db=require('../../fn/db');
 
 module.exports.add=(maquanao,ten,giaca,mausac,loai,gioitinh,malink,size,mainpic,malink1,malink2,malink3,mota)=> {
 
-    var data=db.load("select * from quanao where maquanao = N'"+maquanao+"'")
+   db.load("select * from quanao where maquanao = N'"+maquanao+"'")
         .then(data=>{
             if(data.length>0) {
                 console.log("trung ma quan ao;")
@@ -12,7 +12,7 @@ module.exports.add=(maquanao,ten,giaca,mausac,loai,gioitinh,malink,size,mainpic,
                     "size,mainpic,malink1,malink2,malink3,mota) " +
                     "values ( N'" + maquanao + "', N'" + ten + "'," + giaca + ", N'" + mausac + "', N'" + loai + "',N'" + gioitinh + "', N'"
                     + malink + "',N'" + size + "', N'" + mainpic + "', N'" + malink1 + "', N'" + malink2 + "', N'" + malink3 + "', N'" + mota + "')";
-                db.load(query, function (err, result) {
+                db.load(query,function (err, result) {
                     if (err)
                         console.log(err)
                     else {
@@ -25,3 +25,35 @@ module.exports.add=(maquanao,ten,giaca,mausac,loai,gioitinh,malink,size,mainpic,
 
 
 };
+
+module.exports.update=(req,res)=>
+{
+
+    db.load("select * from quanao where maquanao = N'"+req.body.maquanao+"'")
+        .then(data=>{
+            if(data.length<1) {
+                res.render('admin/quanao_update', {title:'Cập nhật thất bại',layout: 'admin'});
+            }
+            else{
+                var query = "update quanao set ten= N'"+req.body.ten+"' , giaca= "+parseFloat(req.body.giaca)+" , mausac = N'"+req.body.mausac+"' , loai= N'"+req.body.loai+"' , gioitinh= N'"
+                    +req.body.gioitinh+"' , malink = N'"+req.body.mainpic+"' , malink1 = N'"+req.body.malink1
+                +"' , malink2 = N'"+req.body.malink2+"' , malink3 = N'"+req.body.malink3+"' , mota = N'"+req.body.mota+
+                    "' where maquanao = N'"+req.body.maquanao+"'";
+
+                db.load(query);
+
+                    res.render('admin/quanao_update', {title:'Cập nhật thành công',layout: 'admin'});
+
+            }
+
+        });
+
+}
+module.exports.delete=(req,res)=>
+{
+
+    db.load("delete  from quanao where maquanao = N'"+req.body.maquanao+"'")
+        res.render('admin/quanao_delete',{title:"Xóa Thành Công",layout:'admin'});
+
+
+}
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var db=require('../fn/db');
 var ad=require('../models/admin/quanao_model');
 /* GET users listing. */
-router.get('/data', function(req, res, next) {
+// get du lieu bang quan ao
+router.get('/quanao', function(req, res, next) {
 
     var query="select * from quanao";
 
@@ -11,20 +12,62 @@ router.get('/data', function(req, res, next) {
 
         rows=>{
 
-            console.log(rows);
-            res.render('admin/data',{info:rows,layout:'admin'});
+
+            res.render('admin/quanao',{info:rows,layout:'admin'});
         })
 
 });
-router.get('/add', function(req, res, next) {
+// tim kiem bang quan ao
+router.post('/quanao', function(req, res, next) {
+
+    var query="%"+req.body.search+"%";
+    console.log(query);
+    var query="select * from quanao where ten like N'"+query+"' or maquanao like N'"+query+"'";
+
+    db.load(query).then(
+
+        rows=>{
+
+                res.render('admin/quanao', {info: rows, layout: 'admin'});
+
+
+        });
+
+});
+// them du lieu bang quan ao
+router.get('/add_product', function(req, res, next) {
 
             res.render('admin/quanao_create',{title: 'Add Product',layout:'admin'});
 });
-router.post('/add', function(req, res, next) {
+router.post('/add_product', function(req, res, next) {
 
     ad.add(req.body.maquanao,req.body.ten,req.body.giaca,req.body.mausac,req.body.loai,req.body.gioitinh,
         req.body.malink,req.body.size,req.body.mainpic,req.body.malink1,req.body.malink2,req.body.malink3,req.body.mota)
-    res.end();
-});
 
+});
+// updatete bang quan ao
+router.get('/update_product', function(req, res, next) {
+
+               res.render('admin/quanao_update',{title:'Cập nhật sản phẩm',layout:'admin'});
+
+
+});
+router.post('/update_product', function(req, res, next) {
+
+    ad.update(req,res);
+
+});
+// xoa quan ao
+router.get('/delete_product', function(req, res, next) {
+
+    res.render('admin/quanao_delete',{layout:'admin'});
+
+
+});
+router.post('/delete_product', function(req, res, next) {
+
+    ad.delete(req,res);
+
+
+});
 module.exports = router;
